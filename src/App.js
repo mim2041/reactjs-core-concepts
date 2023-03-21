e7619869bdabd5044c232552ac9080988174af41
mim2041 <suraiyamim338@gmail.com>
2023-03-21 23:21:52 +0600

    Basic JS React core concepts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
+const number = 5555;
+const singer = {name: 'Dr. Mahfuz', job: 'single'}
+const singer2 = {name: 'Eva Rahman', job: 'singer2'}
+
+const singerStyle = {
+  color: 'purple',
+  backgroundColor: 'white'
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <h5>New Component. yehhh</h5>
+      <Friend></Friend>
+      <Friend></Friend>
+      <Friend></Friend>
     </div>
   );
+}
+
+function Person(){
+  return (
+    <div className='person'>
+      <h1>Sakib Al Hasan</h1>
+      <p>Profession: cricketer</p>
+    </div>
+  )
+}
+
+function Friend(){
+  return (
+    <div className='container'>
+      <h3>Name: Ajay Devgun</h3>
+      <p>Job: maramari</p>
+    </div>
+  )
 }
 
 export default App;
