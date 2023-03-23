b2cb2a7b58706788b2c311880146c671e639b05d
mim2041 <suraiyamim338@gmail.com>
2023-03-23 19:57:39 +0600

    dynamic data load from array

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 const number = 5555;
-const singer = {name: 'Dr. Mahfuz', job: 'single'}
-const singer2 = {name: 'Eva Rahman', job: 'singer2'}
+const singers = [
+  {name: 'Dr. Mahfuz', job: 'single'},
+  {name: 'Eva Rahman', job: 'singer2'},
+  {name: 'Agun', job: 'sopno'},
+  {name: 'Shuvro', job: 'Pathor'},
+]
 
 const singerStyle = {
   color: 'purple',
@@ -11,35 +15,47 @@ const singerStyle = {
 }
 
 function App() {
+  const nayoks = ['Rubel','BappaRaj', 'Kuber', 'Jashim'];
   return (
     <div className="App">
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
+      {
+        nayoks.map(nayok => <li>{nayok}</li>)
+      }
+      {
+        nayoks.map(nayok => <Person name={nayok}></Person>)
+      }
+
+      {
+        singers.map(singer => <Person name = {singer.name}></Person>)
+      }
+      {/* <Person name={nayoks[0]} nayika="Mousumi"></Person>
+      <Person name={nayoks[1]} nayika="Cheka"></Person>
+      <Person name={nayoks[2]} nayika="Kopila"></Person> */}
       <h5>New Component. yehhh</h5>
+      {/* <Friend movie="Singham"></Friend>
       <Friend></Friend>
-      <Friend></Friend>
-      <Friend></Friend>
-      <Friend></Friend>
+      <Friend></Friend> */}
     </div>
   );
 }
 
-function Person(){
+function Person(props){
+  console.log(props);
   return (
     <div className='person'>
-      <h1>Sakib Al Hasan</h1>
+      <h1>{props.name}</h1>
       <p>Profession: cricketer</p>
     </div>
   )
 }
 
-function Friend(){
+function Friend(props){
+  console.log(props);
   return (
     <div className='container'>
       <h3>Name: Ajay Devgun</h3>
       <p>Job: maramari</p>
+      <p>{props.movie}</p>
     </div>
   )
 }
